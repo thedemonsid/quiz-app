fa266fff06b2b28352f24565efea7297b87d404e
thedemonsid <siddheshshrirame@gmail.com>
2024-09-27 21:41:16 +0530

    :)

--- src/app/teacher/create-quiz/page.tsx
+++ src/app/teacher/create-quiz/page.tsx
@@ -200,7 +200,7 @@ const QuizLengthSelector: React.FC<{
       <Slider
         id="quiz-length"
         min={5}
-        max={30}
+        max={20}
         step={5}
         value={[value]}
         onValueChange={(values: any) => onChange(values[0])}
